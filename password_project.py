258205c6ebfbc4768f9d878f894920dc977daa21
milenamyan <milena.mehrabyan2005@gmail.com>
2025-07-01 03:02:18 +0400

    creating error risers

--- password_project.py
+++ password_project.py
@@ -2,6 +2,11 @@ import string
 import random 
 
 def generate_password(len = 9):
+    if not isinstance(len, int):
+        raise TypeError("Input Error: Password length must be a number.")
+    elif len < 6:
+        raise ValueError("Password length should be at least 6")
+    
     uppercase = string.ascii_uppercase
     lowercase = string.ascii_lowercase
     digits = string.digits
@@ -17,5 +22,4 @@ def generate_password(len = 9):
 
     print(password)
     
-generate_password(int(input()))
 
